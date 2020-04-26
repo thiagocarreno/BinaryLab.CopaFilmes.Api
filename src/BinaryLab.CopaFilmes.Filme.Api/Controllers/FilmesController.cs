c8573a388a8577c82a410b39dd21ae4be17f0263
Thiago Roberto Carreño <Thiago Roberto Carreo>
2020-04-26 14:49:42 -0300

    Criação de projeto de api.

---
+++ src/BinaryLab.CopaFilmes.Filme.Api/Controllers/FilmesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BinaryLab.CopaFilmes.Filme.Api.Controllers
+{
+    //TODO: Implementar Facilitadores
+    //TODO: Implementar Notification Handler
+    //TODO: Implementar Chamadas
+    //TODO: Implementar IOC
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> ObterAsync()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObterVencedoresAsync([FromBody] string[] idsFilmes)
+        {
+            return Ok();
+        }
+    }
+}
